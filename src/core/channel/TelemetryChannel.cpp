f43893d97f6fa89912ea2b896557ba75667e767b
REDMOND\ansuter <ansuter@microsoft.com>
2017-09-20 09:26:24 -0700

    spacing, and update gitignore to discard a bunch of files

--- src/core/channel/TelemetryChannel.cpp
+++ src/core/channel/TelemetryChannel.cpp
@@ -11,23 +11,23 @@
 
 using namespace ApplicationInsights::core;
 
-const int MAX_BUFFER_SIZE = 50;
+const int MAX_BUFFER_SIZE = 100;
 
 /// <summary>
 /// Initializes a new instance of the <see cref="TelemetryChannel"/> class.
 /// </summary>
 /// <param name="config">The configuration.</param>
-TelemetryChannel::TelemetryChannel(TelemetryClientConfig &config) 
-: m_config(&config)
+TelemetryChannel::TelemetryChannel(TelemetryClientConfig &config)
+    : m_config(&config)
 {
-	srand((int)time(0));
-	m_channelId = rand();
-	m_seqNum = 0;
-	m_maxBufferSize = MAX_BUFFER_SIZE;
+    srand((int)time(0));
+    m_channelId = rand();
+    m_seqNum = 0;
+    m_maxBufferSize = MAX_BUFFER_SIZE;
 
 #ifdef WINAPI_FAMILY_PARTITION
 #if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) // Windows phone or store
-	hRespRecv = CreateEventEx(nullptr, L"RecvResp", 0, EVENT_MODIFY_STATE | SYNCHRONIZE);
+    hRespRecv = CreateEventEx(nullptr, L"RecvResp", 0, EVENT_MODIFY_STATE | SYNCHRONIZE);
 #endif
 #endif
 }
@@ -47,39 +47,39 @@ TelemetryChannel::~TelemetryChannel()
 /// <param name="telemetry">The telemetry.</param>
 void TelemetryChannel::Enqueue(TelemetryContext &context, Domain &telemetry)
 {
-	Data data;
-	data.SetBaseData(telemetry);
-	data.SetBaseType(telemetry.GetBaseType());
+    Data data;
+    data.SetBaseData(telemetry);
+    data.SetBaseType(telemetry.GetBaseType());
 
-	std::wstring buffer;
-	StringWriter content(&buffer);
-	JsonWriter json(content);
+    std::wstring buffer;
+    StringWriter content(&buffer);
+    JsonWriter json(content);
 
-	Envelope envelope;
-	envelope.SetData(data);
-	envelope.SetIKey(m_config->GetIKey());
-	envelope.SetTime(Utils::GetCurrentDateTime());
-	envelope.SetName(telemetry.GetEnvelopeName());
-	envelope.SetSeq(std::to_wstring(m_channelId) + L":" + std::to_wstring(m_seqNum++));
+    Envelope envelope;
+    envelope.SetData(data);
+    envelope.SetIKey(m_config->GetIKey());
+    envelope.SetTime(Utils::GetCurrentDateTime());
+    envelope.SetName(telemetry.GetEnvelopeName());
+    envelope.SetSeq(std::to_wstring(m_channelId) + L":" + std::to_wstring(m_seqNum++));
 
-	wstring_wstring_map tags;
-	context.GetContextTags(tags);
-	envelope.SetTags(tags);
+    wstring_wstring_map tags;
+    context.GetContextTags(tags);
+    envelope.SetTags(tags);
 
-	json.WriteObjectValue(&envelope);
-	m_buffer.push_back(content.ToString());
+    json.WriteObjectValue(&envelope);
+    m_buffer.push_back(content.ToString());
 
-	if ((int)m_buffer.size() >= m_maxBufferSize)
-	{
-		Send();
-	}
+    if ((int)m_buffer.size() >= m_maxBufferSize)
+    {
+        Send();
+    }
 
-	if (context.GetSession().GetIsNew().HasValue() && context.GetSession().GetIsNew().GetValue() == L"True")
-	{
-		Nullable<std::wstring> strFalse = std::wstring(L"False");
-		context.GetSession().SetIsFirst(strFalse);
-		context.GetSession().SetIsNew(strFalse);
-	}
+    if (context.GetSession().GetIsNew().HasValue() && context.GetSession().GetIsNew().GetValue() == L"True")
+    {
+        Nullable<std::wstring> strFalse = std::wstring(L"False");
+        context.GetSession().SetIsFirst(strFalse);
+        context.GetSession().SetIsNew(strFalse);
+    }
 }
 
 /// <summary>
@@ -87,50 +87,50 @@ void TelemetryChannel::Enqueue(TelemetryContext &context, Domain &telemetry)
 /// </summary>
 void TelemetryChannel::Send()
 {
-	if (m_buffer.size() > 0)
-	{
-		std::wstring buffer;
+    if (m_buffer.size() > 0)
+    {
+        std::wstring buffer;
 
-		buffer += L"[";
-		for (auto &buf : m_buffer) {
-			buffer += buf + L",";
-		}
-		buffer += L"]";
+        buffer += L"[";
+        for (auto &buf : m_buffer) {
+            buffer += buf + L",";
+        }
+        buffer += L"]";
 
-		m_buffer.clear();
+        m_buffer.clear();
 
 #ifdef CPP_LIB_DEBUG
-		std::wstring req = L"REQUEST :\r\n" + buffer;
-		Utils::WriteDebugLine(req);
+        std::wstring req = L"REQUEST :\r\n" + buffer;
+        Utils::WriteDebugLine(req);
 #endif
 
-		std::vector<std::wstring> in_progress_buffer(m_buffer);
+        std::vector<std::wstring> in_progress_buffer(m_buffer);
 
-		HttpRequest request(HTTP_REQUEST_METHOD::POST, L"dc.services.visualstudio.com", L"/v2/track", buffer);
-		request.GetHeaderFields().SetField(L"Content-Type", L"application/json");
-		request.Send([this, in_progress_buffer](const HttpResponse &response) {
+        HttpRequest request(HTTP_REQUEST_METHOD::POST, L"dc.services.visualstudio.com", L"/v2/track", buffer);
+        request.GetHeaderFields().SetField(L"Content-Type", L"application/json");
+        request.Send([this, in_progress_buffer](const HttpResponse &response) {
 #ifdef CPP_LIB_DEBUG
-			std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-			std::wstring wstrResp = converter.from_bytes(response.GetPayload());
-			
-			std::wstring wstrOutput = L"RESPONSE :\r\n" + wstrResp;
-			Utils::WriteDebugLine(wstrOutput);
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+            std::wstring wstrResp = converter.from_bytes(response.GetPayload());
+
+            std::wstring wstrOutput = L"RESPONSE :\r\n" + wstrResp;
+            Utils::WriteDebugLine(wstrOutput);
 #endif
 
-			if (response.GetErrorCode() >= static_cast<int>(HTTP_RESPONSE_CODE::HTTP_SVR_ERROR))
-			{
-				// reload the buffer if there was some sort of server-side issue
-				m_buffer.insert(m_buffer.begin(), in_progress_buffer.begin(), in_progress_buffer.end());
-			}
+            if (response.GetErrorCode() >= static_cast<int>(HTTP_RESPONSE_CODE::HTTP_SVR_ERROR))
+            {
+                // reload the buffer if there was some sort of server-side issue
+                m_buffer.insert(m_buffer.begin(), in_progress_buffer.begin(), in_progress_buffer.end());
+            }
 
 #ifdef _DEBUG
-			resp = response;
+            resp = response;
 #ifdef WINAPI_FAMILY_PARTITION
 #if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) // Windows phone or store
-			SetEvent(hRespRecv);
+            SetEvent(hRespRecv);
 #endif
 #endif
 #endif
-		});
-	}
+        });
+    }
 }
